b104571f65e29ff8645423d50140e806b433ada1
Rullyan Gabriel dos Santos <rullyan@fintel.com.br>
2024-01-29 21:41:53 -0300

    Adicionar arquivos de projeto.

---
+++ PowerBIWeb.UI.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PowerBIWeb.UI.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
